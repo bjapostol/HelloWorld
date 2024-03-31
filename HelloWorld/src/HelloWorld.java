ec26af03aeeceed9e7164d28e325a9a471b1c56d
Bryan Apostol <apostolbryanjoseph@gmail.com>
2024-03-31 20:09:20 +0800

    - Refresh

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,22 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		String test = " GuEsT               ";
-
-		String input = test;
-
-		System.out.println(input);
-		System.out.println(input.trim().toLowerCase().equals("admin"));
-
-		if (input.equals("admin")) {
-			System.out.println("Hello, Admin!");
-		} else if (input.equals("guest")) {
-			System.out.println("Hello, Guest!");
-		} else {
-			System.out.println("error");
-		}
-		
-		System.out.println("New Merge");
+		System.out.println("New Hello World!");
 
 	}
 }
